e40fe8a8b98d4c0bf2df8d720095c4f071526a3a
simacr <user@10.55.70.52>
2017-04-29 12:23:50 -0400

    Initial one

---
+++ src/camtest.py
@@ -0,0 +1,113 @@
+'''
+webcam testing
+Created on Apr 28, 2017
+Copied from:
+http://www.pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
+
+@author: user
+'''
+
+# import the necessary packages
+import argparse
+import datetime
+#import imutils
+import time
+
+import cv2
+ 
+# initialize the first frame in the video stream
+firstFrame = None
+
+
+def main():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", help="path to the video file")
+    ap.add_argument("-a", "--min-area", type=int, default=200, help="minimum area size")
+    args = vars(ap.parse_args())
+     
+    # if the video argument is None, then we are reading from webcam
+    if args.get("video", None) is None:
+        camera = cv2.VideoCapture(0)  # @UndefinedVariable
+        time.sleep(1)
+     
+    # otherwise, we are reading from a video file
+    else:
+        camera = cv2.VideoCapture(args["video"])  # @UndefinedVariable
+     
+    # initialize the first frame in the video stream
+    firstFrame = None
+    #my cam was starting with blanks
+    camera.read()
+    camera.read()
+    camera.read()
+    i = 0
+    
+    # loop over the frames of the video
+    while True:
+        # grab the current frame and initialize the occupied/unoccupied
+        # text
+        (grabbed, frame) = camera.read()
+        text = "Unoccupied"
+     
+        # if the frame could not be grabbed, then we have reached the end
+        # of the video
+        if not grabbed:
+            break
+     
+        # resize the frame, convert it to grayscale, and blur it
+        #frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # @UndefinedVariable
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)  # @UndefinedVariable
+     
+        # if the first frame is None, initialize it, also initialize every N frames
+        i = i+1
+        if firstFrame is None or i%100==0:
+            firstFrame = gray
+            continue
+
+        # compute the absolute difference between the current frame and
+        # first frame
+        frameDelta = cv2.absdiff(firstFrame, gray) # @UndefinedVariable
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1] # @UndefinedVariable
+     
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2) # @UndefinedVariable
+        (_img, cnts,_hier) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # @UndefinedVariable
+     
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            cntrarea = cv2.contourArea(c) # @UndefinedVariable
+            print cntrarea
+            if cntrarea < args["min_area"]:
+                continue
+     
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x, y, w, h) = cv2.boundingRect(c) # @UndefinedVariable
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) # @UndefinedVariable
+            text = "Occupied"
+
+        # draw the text and timestamp on the frame
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2) # @UndefinedVariable
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1) # @UndefinedVariable
+     
+        # show the frame and record if the user presses a key
+        cv2.imshow("Security Feed", frame) # @UndefinedVariable
+        cv2.imshow("Thresh", thresh) # @UndefinedVariable
+        cv2.imshow("Frame Delta", frameDelta) # @UndefinedVariable
+        key = cv2.waitKey(1) & 0xFF # @UndefinedVariable
+     
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
+     
+    # cleanup the camera and close any open windows
+    camera.release()
+    cv2.destroyAllWindows() # @UndefinedVariable
+    
+    
+if __name__ == "__main__":
+    main()
