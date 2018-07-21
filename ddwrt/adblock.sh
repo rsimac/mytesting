ac26b62c8bcca878d78f308b407a581c30dbd5d2
rsimac <rsimac@users.noreply.github.com>
2018-07-20 21:57:14 -0400

    Added check for nonexisting whitelist
    it was silently failing when whitelist was not existing, all good now

--- ddwrt/adblock.sh
+++ ddwrt/adblock.sh
@@ -19,6 +19,15 @@ URI=http://www.mvps.org/winhelp2002/hosts.txt
 #geo.nbcsports.com
 WHITELIST=/tmp/whitelist.txt
 
+if test -e $WHITELIST
+then
+echo processing $WHITELIST
+else
+echo whitelist empty or not existing, creating $WHITELIST
+touch $WHITELIST
+fi
+
+
 logger Downloading $URI
 wget -O - $URI | grep 0.0.0.0 |
         sed 's/[[:space:]]*#.*$//g;' |
